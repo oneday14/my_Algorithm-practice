1b3682fe8970251d9d9b07306e1624d3802db00b
oneday14 <67682904+oneday14@users.noreply.github.com>
2020-11-21 20:29:58 +0900

    42

---
+++ 하샤드 수.py
@@ -0,0 +1,20 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12947
+
+# 문제 설명
+# 양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다.
+# 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로
+# 18은 하샤드 수입니다. 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는
+# 함수, solution을 완성해주세요.
+
+# 제한 조건
+# x는 1 이상, 10000 이하인 정수입니다.
+
+def solution(x) :
+    each_sum = sum([int(i) for i in str(x)]) 
+    answer = [True if x % each_sum == 0 else False][0]
+    return answer
+
+solution(10)
+solution(12)
+solution(11)
+solution(13)
