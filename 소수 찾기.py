b1c726c16bbd27a80c6d670c204eba756b0f1c64
oneday14 <67682904+oneday14@users.noreply.github.com>
2020-12-09 11:24:39 +0900

    48

--- 소수 찾기.py
+++ 소수 찾기.py
@@ -1,22 +1,47 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42839
+
 # 문제 설명
-# 1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수,
-# solution을 만들어 보세요. 
-# 소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
-# (1은 소수가 아닙니다.)
+# 한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 
+# 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
 
-# 제한 조건
-# - n은 2이상 1000000이하의 자연수입니다.
+# 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 
+# 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
 
-def solution(n):
-    answer = []
-    for i in range(2,n+1) :
-        div = []
-        for j in range(1,i+1) :
-            if i % j == 0 :
-                div.append(j)
-        if len(div) == 2 :
-            answer.append(i)
-    return len(answer)
+# 제한사항
+# - numbers는 길이 1 이상 7 이하인 문자열입니다.
+# - numbers는 0~9까지 숫자만으로 이루어져 있습니다.
+# - '013'은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
 
-solution(10)
-solution(5)
+def solution(numbers):
+    import itertools
+    
+    num = [int(i) for i in numbers]
+    events = []
+    for i in range(1, len(num) + 1) :
+        events.append(list(itertools.permutations(num, i)))
+        
+    string = ''
+    lst = []
+    for i in events :
+        for j in i :
+            for k in j :
+                string += str(k)
+            lst.append(int(string))
+            string = ''
+          
+    lst = list(set(lst))
+    common = []
+    answer = 0
+    for i in lst :
+        if i != 0 and i != 1 :
+            for j in range(1, i//2 + 1) :
+                if i % j == 0 :
+                    common.append(j)
+            if len(common) == 1 :
+                answer += 1
+            common = []
+            
+    return answer
+
+solution('17')
+solution('011')
